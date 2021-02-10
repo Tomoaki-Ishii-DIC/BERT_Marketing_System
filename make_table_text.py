b74448b80e97f674f9d48d0e054a3584cb654d88
Tomoaki Ishii <mlp8a5gh6@gmail.com>
2021-02-10 23:47:38 +0900

    I modified part of the program.

--- make_table_text.py
+++ make_table_text.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 
+import preprocessing
+
 csv_path_news = ("./datasets/x_train.csv")
 
 # ニュース記事
@@ -46,82 +48,12 @@ import sentencepiece as spm
 import numpy as np
 
 
-def preprocessing_text(text):
-    '''
-    前処理
-    '''
-    print(text)
-    # 改行コードを消去
-    text = re.sub('<br />', '', text)
-    text = re.sub('\n', '', text)
+# preprocessing_text 削除
 
-    # カンマ、ピリオド以外の記号をスペースに置換
-    for p in string.punctuation:
-        if (p == ".") or (p == ","):
-            continue
-        else:
-            text = text.replace(p, " ")
+# tokenizer_mecab 削除
 
-    for p in string.punctuation:
-        if (p == "。") or (p == "、"):
-            continue
-        else:
-            text = text.replace(p, "　")
+# 関数移動　_get_indice
 
-    # ピリオドなどの前後にはスペースを入れておく
-    text = text.replace(".", " . ")
-    text = text.replace(",", " , ")
-    text = text.replace("。", " 。 ")
-    text = text.replace("、", " 、 ")
-
-    return text
-
-#m_t = MeCab.Tagger('-Owakati -d /usr/lib/mecab/dic/mecab-ipadic-neologd')
-#m_t = MeCab.Tagger('-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
-wakati = MeCab.Tagger("-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-
-def tokenizer_mecab(text):
-    '''
-    分かち書き
-    '''
-    words = wakati.parse(text).split()
-
-    return words
-
-
-
-# ここでもsentence piece
-sp = spm.SentencePieceProcessor()
-sp.Load('./downloads/bert-wiki-ja/wiki-ja.model')
-
-
-# 最大単語数分のID化された文を返す関数
-# maxlenがなくてエラーになるので勝手に追加（maxlenは最大単語数か？）
-def _get_indice(feature, maxlen):
-#def _get_indice(feature):
-    # インデックス ０で埋める
-    indices = np.zeros((maxlen), dtype = np.int32)
-    # 最初に[CLS]、最後に'[SEP]をつけてトークン作る
-    tokens = []
-    tokens.append('[CLS]')
-    pre_text = preprocessing_text(feature)#追加
-    tokenized_text = tokenizer_mecab(pre_text)#追加
-    tokens.extend(tokenized_text)#追加
-    #tokens.extend(sp.encode_as_pieces(feature))# sentence piece
-    tokens.append('[SEP]')
-
-    for t, token in enumerate(tokens):
-        # 最大単語数までトークンの単語をindicesに入れていく
-        if t >= maxlen:
-            break
-        try:
-            indices[t] = sp.piece_to_id(token)# id化してくれる？
-        except:
-            logging.warn(f'{token} is unknown.')# コメントしてくれる
-            indices[t] = sp.piece_to_id('<unk>')# id化してくれる？unknown
-
-    # 最大単語数分のID化された文を返す
-    return indices
 
 # ベクトルをdfに
 # ID変換処理(今回はテキストのみ)
@@ -133,9 +65,9 @@ df_news = pd.DataFrame([])
 
 for i in range(len(df_s)):
     text = df_s.loc[i]["text"]
-    pre_text = preprocessing_text(text)
-    tokenized_text = tokenizer_mecab(pre_text)
-    indice = _get_indice(text , maxlen=TEXT_LEN)
+    pre_text = preprocessing.preprocessing_text(text)
+    tokenized_text = preprocessing.tokenizer_mecab(pre_text)
+    indice = preprocessing._get_indice(text , maxlen=TEXT_LEN)
     df_text_temp = pd.DataFrame(indice).T
 
     df_news = pd.concat([df_news, df_text_temp], ignore_index=True)#, axis = 1
